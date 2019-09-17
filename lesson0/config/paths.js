e1fe3b4f087a904f108cb1c0414359d687ef5e27
kakanis <sreedhara.kakani@equiniti.com>
2019-09-17 11:28:58 +0100

    watching for new screens now

--- lesson0/config/paths.js
+++ lesson0/config/paths.js
@@ -10,7 +10,7 @@ const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 module.exports = {
-  appAssets: resolveApp('src/assets'), // For images and other assets
+  appAssets: resolveApp('src/public'), // For images and other assets
   appBuild: resolveApp('build'), // Prod built files end up here
   appConfig: resolveApp('config'), // App config files
   appScreens: path.join('src', 'screens', path.sep),
--- lesson0/config/webpack-dev-server.js
+++ lesson0/config/webpack-dev-server.js
@@ -1,15 +1,20 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 // webpack-dev-server.js
 
 // dev-server is responsible for running your project locally
 
 const WebpackDevServer = require('webpack-dev-server');
 const webpack = require('webpack');
+const chokidar = require('chokidar');
 const paths = require('./paths');
-const config = require('./webpack-dev-config.js');
 
 // Change port to suit your preference
 const Port = 3000;
 const Host = 'localhost';
+
+const WP_COMMON_CONFIG = './webpack-common-config.js';
+const WP_DEV_CONFIG = './webpack-dev-config.js';
 
 let server;
 
@@ -17,6 +22,7 @@ const options = {
   host: Host,
   // Enable webpack's Hot Module Replacement feature
   hot: true,
+  inline : true,
   // full-screen overlay in the browser for compiler errors or warnings
   overlay: {
     warnings: false,
@@ -38,8 +44,23 @@ const options = {
   },
 };
 
-WebpackDevServer.addDevServerEntrypoints(config, options);
-const compiler = webpack(config);
-server = new WebpackDevServer(compiler, options);
+function startDevServer(config) {
+  WebpackDevServer.addDevServerEntrypoints(config, options);
+  const compiler = webpack(config);
+  server = new WebpackDevServer(compiler, options);
 
-server.listen(Port, Host, () => {});
+  server.listen(Port, Host, () => {});
+}
+
+startDevServer(require(WP_DEV_CONFIG));
+
+chokidar
+  .watch(paths.appScreens, { ignoreInitial: true, ignored: '*.html' })
+  .on('all', () => {
+    server.close();
+
+    // bust config from cache so we get fresh configuration to load
+    delete require.cache[require.resolve(WP_DEV_CONFIG)];
+    delete require.cache[require.resolve(WP_COMMON_CONFIG)];
+    server = startDevServer(require(WP_DEV_CONFIG));
+  });
