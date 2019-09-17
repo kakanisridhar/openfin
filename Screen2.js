2d4b3cb7fd2f847feb8b109731566abc8eaf2766
kakanis <sreedhara.kakani@equiniti.com>
2019-09-17 13:28:46 +0100

    removing junk

--- Screen2.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Screen2 = params => (
-  <p>This is a simple screen for testing continuous compilation</p>
-);
-
-ReactDOM.render(<Screen2 />, document.getElementById('root'));
